f7828332b290d1b72e7b93c7325399654ab14225
jackson vargas <jacksonvargas134@gmail.com>
2026-03-24 09:52:14 -0300

    remoção do componente map

--- src/components/contact/index.tsx
+++ src/components/contact/index.tsx
@@ -1,5 +1,4 @@
 import { Container } from '@/components/container'
-import { Map } from './map'
 import { icons } from './icons-contact'
 
 export function Contact() {
@@ -31,7 +30,13 @@ export function Contact() {
           </div>
         </div>
 
-        <Map />
+        <iframe
+          src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d220895.17940008777!2d-51.17714195!3d-30.108870050000007!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95199cd2566acb1d%3A0x603111a89f87e91f!2sPorto%20Alegre%2C%20RS!5e0!3m2!1spt-BR!2sbr!4v1772222427201!5m2!1spt-BR!2sbr'
+          loading='lazy'
+          allowFullScreen
+          referrerPolicy='no-referrer-when-downgrade'
+          className='w-full h-full rounded'
+        />
       </section>
     </Container>
   )
